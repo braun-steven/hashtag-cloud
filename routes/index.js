cc040adac863202fed74ae21bf534925759174fe
slang03 <slang03@students.uni-mainz.de>
2016-09-07 11:43:36 +0200

    Initial upload

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var neo4j = require('neo4j');
+var neoUser = 'neo4j';
+var neoPw = 'admin';
+var neoHost = 'localhost';
+var neoPort = '7474';
+var neoUrl = `http://${neoUser}:${neoPw}@${neoHost}:${neoPort}`;
+var db = new neo4j.GraphDatabase(neoUrl);
+
+/* Post query and return neo4j results. */
+router.post('/query', function (req, res, next) {
+    // Cypherquery
+    var query = req.body.query;
+    db.cypher({
+        query: query,
+        params: {}
+    }, function (err, results) {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+module.exports = router;
